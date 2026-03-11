a50d560156f60f29b1cf83d1630851f678453ef7
KarlLbs <karl.lubbos@ensea.fr>
2026-03-11 10:31:19 +0100

    Improve and correct training loop and Q-table update logic, handle edge cases

--- agents/q_learning_agent.py
+++ agents/q_learning_agent.py
@@ -14,9 +14,14 @@ class QLearningAgent(BaseAgent):
     def chose_action(self, grid):
         grid = tuple(grid)
         if self.mode == "exploitation":
-            possible_actions = self.qtable[grid]
-            action = max(possible_actions, key=possible_actions.get)
+            if grid in self.qtable : 
+                possible_actions = self.qtable[grid]
+                action = max(possible_actions, key=possible_actions.get)
+            else : 
+                possible_actions = [i for i in range(9) if grid[i] is None]
+                action = random.choice(possible_actions)
             return action
+
         if self.mode == "exploration":
             if grid in self.qtable:
                 possible_actions = self.qtable[grid]
@@ -27,18 +32,19 @@ class QLearningAgent(BaseAgent):
 
     def update_qtable(self, grid, action, reward, new_grid):
         #print("previous qtable : ", self.qtable)
-        grid, new_grid = tuple(grid), tuple(new_grid)
+        grid = tuple(grid)
         if grid not in self.qtable :
             #self.qtable[grid] = [0 if grid[i] is None else float('-inf') for i in range(9)]
             self.qtable[grid] = {i : 0 for i in range(9) if grid[i] is None}
-        if new_grid not in self.qtable : 
-            self.qtable[new_grid] = {i : 0 for i in range(9) if new_grid[i] is None}
         
-        future_actions = self.qtable[new_grid]
-        if future_actions :
-            max_future_q = max(future_actions, key=future_actions.get)
-        else : 
-            max_future_q = 0
-        Q_new = reward + self.df * max_future_q
+        Q_new = reward
+        if new_grid :
+            new_grid = tuple(new_grid)
+            if new_grid not in self.qtable : 
+                self.qtable[new_grid] = {i : 0 for i in range(9) if new_grid[i] is None}
+            future_actions = self.qtable[new_grid]
+            max_future_q = max(future_actions.values())
+            Q_new += self.df * max_future_q
+
         self.qtable[grid][action] += self.lr * (Q_new - self.qtable[grid][action])
         #print("new qtable : ", self.qtable)
--- main.py
+++ main.py
@@ -51,28 +51,48 @@ def game_engine():
     engine.play_x_games_fair(10000)
 
 def training_loop():
+    game = GameState()
     agent = QLearningAgent(mode="exploration")
     opponent = RandomAgent()
-    game = GameState()
-    nb_episodes = 100000
+
+    # Before training
+    print("Before training :")
+    engine = GameEngine(game, agent, opponent)
+    engine2 = GameEngine(game, opponent, agent)
+    engine.play_x_games(10000)
+    engine2.play_x_games(10000)
+
+    nb_episodes = 50000
     for _ in range(nb_episodes):
         grid = game.reset()
         done_flag = False
         while not done_flag:
+            # Playing turns
             action = agent.chose_action(grid)
             new_grid, done_flag = game.step(action)
             #game.render()
             if not done_flag : 
                 new_grid, done_flag = game.step(opponent.chose_action(new_grid))
                 #game.render()
-            if game.detect_end()==1 : 
+            # Reward calculation and Q-table update
+            if game.detect_end()==1 : # A player has won
                 reward = 10 if game.whos_turn_is_it()==2 else -10
-            else : 
+                agent.update_qtable(grid, action, reward, None)
+            elif game.detect_end()==0 : # Tie
                 reward = 0
-            #print("grid, action, reward, new_grid : ", grid, action, reward, new_grid)
-            agent.update_qtable(grid, action, reward, new_grid)
+                agent.update_qtable(grid, action, reward, None)
+            else : # Game not over
+                reward = 0 
+                agent.update_qtable(grid, action, reward, new_grid)
             grid = new_grid
-    print(agent.qtable[tuple([None for _ in range(9)])])
+
+    #print(agent.qtable[tuple([None for _ in range(9)])])
+
+    # After training
+    print("\nAfter training :")
+    agent.mode = "exploitation"
+    engine.play_x_games(10000)
+    engine2.play_x_games(10000)
 
 if __name__ == "__main__":
     training_loop()
